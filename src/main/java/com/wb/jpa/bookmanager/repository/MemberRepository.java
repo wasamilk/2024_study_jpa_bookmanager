190c412cf2ef81bb71e392b99c44e5f3a1ba2280
wasamilk <vin5004@naver.com>
2024-01-22 22:38:04 +0900

    [24.01.22] 쿼리메소드 테스트1

--- src/main/java/com/wb/jpa/bookmanager/repository/MemberRepository.java
+++ src/main/java/com/wb/jpa/bookmanager/repository/MemberRepository.java
@@ -5,9 +5,31 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 // JpaRepository<엔티티, 엔티티의 pk>
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    /** 쿼리메소드의 리턴값은 다양한 타입 가능 ex)Optional, Set, List... */
+//    Optional<Member> findByName(String name);
+//    Set<Member> findByName(String name);
     List<Member> findByName(String name);
+
+    /** find...By, get..By 등 모두 같은 동작이므로 코드 가독성에 맞춰서 메소드이름 택1,
+     *  ...에 엔티티(사실 엔티티 아니고 아무거나넣어도 됨)넣어도 같은동작 */
+    Member findByEmail(String email);
+    Member getByEmail(String email);
+    Member readByEmail(String email);
+    Member queryByEmail(String email);
+    Member searchByEmail(String email);
+    Member streamByEmail(String email);
+    Member findMemberByEmail(String email);
+    Member findSomethingByEmail(String email);
+
+    /** First == Top  ,  Last는 제공 X */
+    Member findFirst1ByName(String name);
+    Member findTop1ByName(String name);
+    List<Member> findFirst2ByName(String name);
+    List<Member> findTop2ByName(String name);
 }
--- src/test/java/com/wb/jpa/bookmanager/repository/MemberRepositoryTest2.java
+++ src/test/java/com/wb/jpa/bookmanager/repository/MemberRepositoryTest2.java
@@ -1,5 +1,6 @@
 package com.wb.jpa.bookmanager.repository;
 
+import com.wb.jpa.bookmanager.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +11,26 @@ class MemberRepositoryTest2 {
     private MemberRepository memberRepository;
 
     @Test
-    void select(){
+    void basic(){
+        // 쿼리메소드의 리턴값은 다양한 타입 가능 ex)Optional, Set, List...
         System.out.println(memberRepository.findByName("martin"));
+
+        System.out.println(memberRepository.findByEmail("martin@wasamilk.com"));
+        System.out.println(memberRepository.getByEmail("martin@wasamilk.com"));
+        System.out.println(memberRepository.readByEmail("martin@wasamilk.com"));
+        System.out.println(memberRepository.queryByEmail("martin@wasamilk.com"));
+        System.out.println(memberRepository.searchByEmail("martin@wasamilk.com"));
+        System.out.println(memberRepository.streamByEmail("martin@wasamilk.com"));
+        System.out.println(memberRepository.findMemberByEmail("martin@wasamilk.com"));
+        System.out.println(memberRepository.findSomethingByEmail("martin@wasamilk.com"));
+
+        System.out.println("findFirst1ByName >> " + memberRepository.findFirst1ByName("martin"));
+        System.out.println("findTop1ByName >> " + memberRepository.findTop1ByName("martin"));
+        System.out.println("findFirst2ByName >> " + memberRepository.findFirst2ByName("martin"));
+        System.out.println("findTop2ByName >> " + memberRepository.findTop2ByName("martin"));
+    }
+    
+    @Test
+    void basic2(){
     }
 }
