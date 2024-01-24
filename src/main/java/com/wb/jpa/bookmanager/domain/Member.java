8bc512d1b56fa9f0c268ce80a235f250f1fd9035
wasamilk <vin5004@naver.com>
2024-01-24 21:13:28 +0900

    [24.01.24] JPA 연관관계 학습 전 소스 정리

--- src/main/java/com/wb/jpa/bookmanager/domain/Member.java
+++ src/main/java/com/wb/jpa/bookmanager/domain/Member.java
@@ -6,12 +6,7 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-/* ---- @Data ---- */
-//@Setter
-//@Getter
-//@ToString
-//@EqualsAndHashCode
-/* --------------- */
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,7 +17,6 @@ import java.time.LocalDateTime;
         indexes = {@Index(columnList = "name")},
         uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})})
 @EntityListeners(value = MemberEntityListener.class)
-//@EntityListeners(value = {MyEntityListener.class, MemberEntityListener.class})
 public class Member {
 
     @Id
@@ -49,39 +43,5 @@ public class Member {
     private String testData;
 //    @OneToMany(fetch = FetchType.EAGER)
 //    private List<Address> address;
-
-    // pre-before, post-after
-    @PrePersist
-    public void prePersist(){
-        this.createdAt=LocalDateTime.now();
-        this.updatedAt=LocalDateTime.now();
-        System.out.println(">>> prePersist");
-    }
-    @PreUpdate
-    public void preUpdate(){
-        this.updatedAt=LocalDateTime.now();
-        System.out.println(">>> preUpdate");
-    }
-    @PreRemove
-    public void preRemove(){
-        System.out.println(">>> preRemove");
-    }
-    @PostPersist
-    public void postPersist(){
-        System.out.println(">>> postPersist");
-    }
-    @PostUpdate
-    public void postUpdate(){
-        System.out.println(">>> postUpdate");
-    }
-    @PostRemove
-    public void postRemove(){
-        System.out.println(">>> postRemove");
-    }
-    @PostLoad
-    public void postLoad(){
-        System.out.println(">>> postLoad");
-    }
-
 
 }
--- src/main/java/com/wb/jpa/bookmanager/domain/MemberHistory.java
+++ src/main/java/com/wb/jpa/bookmanager/domain/MemberHistory.java
@@ -1,29 +1,21 @@
 package com.wb.jpa.bookmanager.domain;
 
-import com.wb.jpa.bookmanager.domain.listener.Auditable;
-import com.wb.jpa.bookmanager.domain.listener.MyEntityListener;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @NoArgsConstructor
 @Data
-@EntityListeners(value = MyEntityListener.class)
-public class MemberHistory implements Auditable {
+public class MemberHistory extends BaseEntity{
     @Id
     @GeneratedValue
     private Long id;
 
-    private Long MemberId;
+    private Long memberId;
 
     private String name;
 
     private String email;
 
-    private LocalDateTime createdAt;
-
-    private LocalDateTime updatedAt;
 }
